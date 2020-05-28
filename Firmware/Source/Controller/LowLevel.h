f994d53dacdb696798c0dbc51fa05264ff800533
asytyi <alexander.a.sytyi@gmail.com>
2020-05-28 14:21:41 +0300

    Исправлены заголовки функций в LowLevel.h

--- Firmware/Source/Controller/LowLevel.h
+++ Firmware/Source/Controller/LowLevel.h
@@ -2,35 +2,18 @@
 #define __LOWLEVEL_H
 
 // Include
-#include "ZwGPIO.h"
+#include "stdinc.h"
 
 // Functions
 //
-
-bool LL_IsBlinkLED();
-void LL_BlinkLED(bool State);
-//void LL_ExternalLED(bool State);
-//void LL_TimersReset(bool State);
-void LL_ExternalFan(bool State);
-//void LL_ExternalSync(bool State);
-//void LL_GateControl(bool State);
-//void LL_RelayControl(bool State);
-//void LL_GateLatch(bool State);
-//bool LL_IsShiftRegData();
-//void LL_ShiftRegLoad(bool State);
-//void LL_ShiftRegClk(bool State);
-//void LL_ShiftRegCS(bool State);
-//void LL_TOCUSerialEnable(bool State);
-//void LL_TOCUSerialSRCK(bool State);
-//void LL_TOCUSerialRCK(bool State);
-//void LL_TOCUSerialData(bool State);
-
-void LL_CapChargeDischarge(bool State);
-void LL_RCK(bool State);
-void LL_SRCK(bool State);
-void LL_LedPower(bool State);
-void LL_PowerRelay(bool State);
-void LL_PSBoard(bool State);
-
+void LL_ToggleBoardLED();
+void LL_Fan(bool State);
+void LL_BatteryDischarge(bool State);
+void LL_ExternalLED(bool State);
+void LL_MeanWellRelay(bool State);
+void LL_PSBoardOutput(bool State);
+void LL_SoftSpiSRCK(bool State);
+void LL_SoftSpiRCK(bool State);
+void LL_SoftSpiData(bool State);
 
 #endif //__LOWLEVEL_H
